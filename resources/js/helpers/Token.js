739879ee875a544d5b6f9a2a78811f23078381a3
fauzanmzlm <fauzanmazlam88@gmail.com>
2023-07-27 22:17:34 +0800

    update comment in file token.js

--- resources/js/helpers/Token.js
+++ resources/js/helpers/Token.js
@@ -1,5 +1,8 @@
+//This file defines a JavaScript class called "Token," which provides methods related to token validation and decoding.
+
 class Token {
 
+    // The "isValid" method checks if the token is valid by decoding it and verifying the "iss" (issuer) claim against specific endpoints, "/api/auth/login" or "/api/auth/signup."
     isValid (token) {
         const payload = this.payload(token)
 
@@ -10,12 +13,14 @@ class Token {
         return false
     }
 
+    // This method decodes the token and returns the payload as a JSON object.
     payload (token) {
         const payload = token.split('.')[1]
         
         return this.decode(payload)
     }
 
+    // This method decodes the token's payload using Base64 decoding and returns the parsed JSON.
     decode (payload) {
         return JSON.parse(atob(payload))
     }
