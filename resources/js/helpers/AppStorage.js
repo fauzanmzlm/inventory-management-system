428da9a2cbcd9495df98e5e4662ec32ea1669d35
fauzanmzlm <fauzanmazlam88@gmail.com>
2023-07-27 22:21:08 +0800

    update comment

--- resources/js/helpers/AppStorage.js
+++ resources/js/helpers/AppStorage.js
@@ -1,9 +1,13 @@
+// This file defines a JavaScript class called "AppStorage," which handles storing and retrieving the user's token and name in local storage.
+
 class AppStorage {
 
+    // Store Token
     storeToken (token) {
         localStorage.setItem('token', token)
     }
 
+    // Store User
     storeUser (user) {
         localStorage.setItem('user', user)
     }
@@ -13,15 +17,18 @@ class AppStorage {
         this.storeUser(user)
     }
 
+    // This method removes the token and user from local storage.
     clear () {
         localStorage.removeItem('token')
         localStorage.removeItem('user')
     }
 
+    // Retrieve Token
     getToken () {
         localStorage.getItem('token');
     }
 
+    // Retrieve User
     getUser () {
         localStorage.getItem('user');
     }
