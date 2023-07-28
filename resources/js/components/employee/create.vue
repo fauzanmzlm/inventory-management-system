25e6d67382e2852b3e645d72d32aca576dfe7776
fauzanmzlm <fauzanmazlam88@gmail.com>
2023-07-28 12:47:53 +0800

    create add employee component

---
+++ resources/js/components/employee/create.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h1>Employee</h1>
+    </div>
+</template>
+
+<script type="text/javascript">
+    export default {
+
+    }
+</script>
+
+<style type="text/css">
+
+</style>
