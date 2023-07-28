e8f87e8fef669a99a12fd62fb08dd3c8227d008c
fauzanmzlm <fauzanmazlam88@gmail.com>
2023-07-28 12:46:51 +0800

    add new logout component

---
+++ resources/js/components/auth/logout.vue
@@ -0,0 +1,24 @@
+<template>
+
+</template>
+
+<script type="text/javascript">
+    export default {
+        created() {
+            // Remove Token & User
+            localStorage.removeItem('token')
+            localStorage.removeItem('user')
+            // Display alert
+            Toast.fire({
+                icon: 'success',
+                title: 'Logout successfully'
+            })
+            // Redirect to Login Page
+            this.$router.push({ name: '/' })
+        }
+    }
+</script>
+
+<style type="text/css">
+
+</style>
