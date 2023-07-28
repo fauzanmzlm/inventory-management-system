317078fae0660f0020aadcb7091643f8cc278993
fauzanmzlm <fauzanmazlam88@gmail.com>
2023-07-28 12:48:52 +0800

    add a few routes

--- resources/js/routes.js
+++ resources/js/routes.js
@@ -1,11 +1,22 @@
-let login = require('./components/auth/login.vue').default;
+let Login = require('./components/auth/login.vue').default;
 let register = require('./components/auth/register.vue').default;
 let forget = require('./components/auth/forget.vue').default;
+let logout = require('./components/auth/logout.vue').default;
+
+// End Authentication
+
 let home = require('./components/home.vue').default;
+
+// Employee Component
+let storeEmployee = require('./components/employee/create.vue').default;
 
 export const routes = [
     { path: '/', component: login, name: '/' },
     { path: '/register', component: register, name: 'register' },
     { path: '/forget', component: forget, name: 'forget' },
+    { path: '/logout', component: logout, name: 'logout' },
     { path: '/home', component: home, name: 'home' },
+    
+    // Employee
+    { path: '/store-employee', component: storeEmployee, name: 'store-employee' },
 ]
