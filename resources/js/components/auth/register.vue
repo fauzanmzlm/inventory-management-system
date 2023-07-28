64f24bd2cb2d7d45dac71d8e3087b554afaa36e6
fauzanmzlm <fauzanmazlam88@gmail.com>
2023-07-28 12:47:14 +0800

    register process

--- resources/js/components/auth/register.vue
+++ resources/js/components/auth/register.vue
@@ -10,22 +10,34 @@
                                     <div class="text-center">
                                         <h1 class="h4 text-gray-900 mb-4">Register</h1>
                                     </div>
-                                    <form>
+                                    <form class="user" @submit.prevent="register">
                                         <div class="form-group">
-                                            <input type="text" class="form-control" id="full_name" name="full_name"
-                                                placeholder="Enter Full Name">
+                                            <input type="text" class="form-control" id="name"
+                                                placeholder="Enter Full Name" v-model="form.name">
+                                            <small v-if="errors.name" class="text-danger">
+                                                {{ errors.name[0] }}
+                                            </small>
                                         </div>
                                         <div class="form-group">
-                                            <input type="email" class="form-control" id="email" name="email"
-                                                aria-describedby="emailHelp" placeholder="Enter Email Address">
+                                            <input type="email" class="form-control" id="email"
+                                                aria-describedby="emailHelp" placeholder="Enter Email Address" v-model="form.email">
+                                            <small v-if="errors.email" class="text-danger">
+                                                {{ errors.email[0] }}
+                                            </small>
                                         </div>
                                         <div class="form-group">
-                                            <input type="password" class="form-control" id="password" name="password"
-                                                placeholder="Enter Password">
+                                            <input type="password" class="form-control" id="password"
+                                                placeholder="Enter Password" v-model="form.password">
+                                                <small v-if="errors.password" class="text-danger">
+                                                {{ errors.password[0] }}
+                                            </small>
                                         </div>
                                         <div class="form-group">
-                                            <input type="password" class="form-control" id="confirm_password" name="confirm_password"
-                                                placeholder="Enter Confirm Password">
+                                            <input type="password" class="form-control" id="password_confirmation"
+                                                placeholder="Enter Confirm Password" v-model="form.password_confirmation">
+                                                <small v-if="errors.password_confirmation" class="text-danger">
+                                                {{ errors.password_confirmation[0] }}
+                                            </small>
                                         </div>
                                         <div class="form-group">
                                             <button type="submit" class="btn btn-primary btn-block">Register</button>
@@ -34,7 +46,8 @@
                                     </form>
                                     <hr>
                                     <div class="text-center">
-                                        <router-link to="/" class="font-weight-bold small">Already have an account?</router-link>
+                                        <router-link to="/" class="font-weight-bold small">Already have an
+                                            account?</router-link>
                                     </div>
                                 </div>
                             </div>
@@ -46,7 +59,40 @@
     </div>
 </template>
 
-<script type="text/javascript"></script>
+<script type="text/javascript">
+    export default {
+        created () {
+            if (User.loggedIn()) {
+                this.$router.push({ name: 'home' })
+            }
+        },
+        data () {
+            return {
+                form: {
+                    name: null,
+                    email: null,
+                    password: null,
+                    confirm_password: null,
+                },
+                errors: {}
+            }
+        },
+        methods: {
+            register () {
+                axios.post('/api/auth/signup', this.form)
+                    .then(response => {
+                        User.responseAfterLogin(response)
+                        Toast.fire({
+                            icon: 'success',
+                            title: 'Signed in successfully'
+                        })
+                        this.$router.push({ name: 'home' })
+                    })
+                    .catch(error => this.errors = error.response.data.errors)
+            }
+        }
+    }
+</script>
 
 <style type="text/css">
 
