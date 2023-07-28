2886f87c5ed3bf3f95db4d014349a6a0c1c81cfa
fauzanmzlm <fauzanmazlam88@gmail.com>
2023-07-28 08:39:05 +0800

    add new condition, if user not logged-in user will redirected to login page

--- resources/js/components/home.vue
+++ resources/js/components/home.vue
@@ -321,7 +321,15 @@
     </div>
 </template>
 
-<script type="text/javascript"></script>
+<script type="text/javascript">
+    export default {
+        created () {
+            if (!User.loggedIn()) {
+                this.$router.push({ name: '/' })
+            }
+        }
+    };
+</script>
 
 <style type="text/css">
 
