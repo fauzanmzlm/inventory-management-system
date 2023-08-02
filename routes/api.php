217e1425d7bd0dd87fd4bf23511b2fcb479c0dd9
fauzanmzlm <fauzanmazlam88@gmail.com>
2023-08-02 14:39:40 +0800

    Create CRUD for Supplier Module

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\Api\EmployeeController;
+use App\Http\Controllers\Api\SupplierController;
 
 // Authentication
 Route::group([
@@ -20,5 +21,5 @@ Route::group([
 
 });
 
-// Employee
 Route::apiResource('/employee', EmployeeController::class);
+Route::apiResource('/supplier', SupplierController::class);
