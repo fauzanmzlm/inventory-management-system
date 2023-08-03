aed6dd8c94736377aa80b46390493fc829bc7dbb
fauzanmzlm <fauzanmazlam88@gmail.com>
2023-08-03 17:09:15 +0800

    add footer into main layout file

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -230,7 +230,18 @@
                         <router-view></router-view>
                     </div>
                     <!---Container Fluid-->
-                </div>
+                </div> 
+                <!-- Footer -->
+                <footer class="sticky-footer bg-white">
+                    <div class="container my-auto">
+                    <div class="copyright text-center my-auto">
+                        <span>copyright &copy; <script> document.write(new Date().getFullYear()); </script> - developed by
+                        <b><a href="https://indrijunanda.gitlab.io/" target="_blank">indrijunanda</a></b>
+                        </span>
+                    </div>
+                    </div>
+                </footer>
+                <!-- Footer -->
             </div>
         </div>
     </div>
